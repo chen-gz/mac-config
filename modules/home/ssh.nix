9d9bfa8e4d4aad381ddc7355efaeb079a115a3e1
GG Chen <ggzongchen@gmail.com>
2026-01-16 09:52:21 -0800

    update ssh setting

--- modules/home/ssh.nix
+++ modules/home/ssh.nix
@@ -1,7 +1,19 @@
 { pkgs, ... }:
 {
+  # 确保 sockets 目录存在
+  home.file.".ssh/sockets/.keep".text = "";
+
   programs.ssh = {
     enable = true;
-    addKeysToAgent = "yes";
+    enableDefaultConfig = false;
+    
+    matchBlocks."*" = {
+      addKeysToAgent = "yes";
+      extraOptions = {
+        ControlMaster = "auto";
+        ControlPersist = "24h";
+        ControlPath = "~/.ssh/sockets/%r@%h-%p";
+      };
+    };
   };
 }
