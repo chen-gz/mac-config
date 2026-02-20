252e6fe8ae00774650f47a780268fd31aa03c044
Guangzong Chen <guangzong@google.com>
2026-02-20 09:14:08 -0800

    update gpg setup

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -7,12 +7,17 @@
       user = {
         name = "Guangzong Chen";
         email = "guangzong@google.com";
+        signingkey = "20AE4BA8FF696FB5E21AE9D0636538D58AF1006D";
       };
       core = {
         sshCommand = "/usr/bin/ssh";
       };
       commit = {
         signoff = true;
+        gpgsign = true;
+      };
+      pull = {
+        rebase = true;
       };
       color = {
         ui = "auto";
