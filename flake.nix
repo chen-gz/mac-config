3948199161977a0e91712c96f03a8e79fc3e93ef
Guangzong Chen <guangzong@google.com>
2026-09-16 16:23:13 -0700

    fix(flake): use nixfmt-tree as formatter to prevent bootstrap format hanging on stdin

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
     in
     {
       # 配置格式化工具
-      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt);
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-tree);
 
       darwinConfigurations = {
         "gg-mac-mini" = nix-darwin.lib.darwinSystem {
